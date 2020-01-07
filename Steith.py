fd8a89b2e2fe6d7bc8d9061e2362155801a11b4f
techiecream <34246365+techiecream@users.noreply.github.com>
2020-01-07 16:05:42 +0300

    Update Steith.py

--- Steith.py
+++ Steith.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python2.7
 #My Personal Assistant
+#Possible Features to adds
+#Check emails
+#Reply emails
+#Send emails
+#Check news
+#Post to Facebook / twitter / telegram / WhatsApp
+#Read SMS
+#Download YouTube videos
+#Summarise articles
+#Read articles
+#Set reminders / schedule tasks
+#Check past events
+#Google search
+#Download files
+#Recommend movies, songs or articles
+#Send bulk SMS
