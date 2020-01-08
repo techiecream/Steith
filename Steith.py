a74aabffebf770e335d6929cf2ba716df53376e2
techiecream <34246365+techiecream@users.noreply.github.com>
2020-01-08 20:49:22 +0300

    Update Steith.py

--- Steith.py
+++ Steith.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python2.7
+import smtplib
+import time
+import imaplib
+import email
+import imaplib
+import base64
+import os
+import email
 #My Personal Assistant
 #Possible Features to adds
 #Check emails
@@ -16,3 +24,28 @@
 #Download files
 #Recommend movies, songs or articles
 #Send bulk SMS
+def CheckNewEmails():
+    try:
+        mail = imaplib.IMAP4_SSL('imap.gmail.com','993')
+        mail.login('jamiecmacleod@gmail.com','txqiydpihfmoekzb')
+        mail.select('inbox')
+        type, data = mail.search(None, 'UNSEEN')
+        mail_ids = data[0]
+
+        id_list = mail_ids.split()   
+        first_email_id = int(id_list[0])
+        latest_email_id = int(id_list[-1])
+        for i in range(latest_email_id,first_email_id, -1):
+            typ, data = mail.fetch(i, '(RFC822)' )
+            for response_part in data:
+                if isinstance(response_part, tuple):
+                    msg = email.message_from_string(response_part[1])
+                    email_subject = msg['subject']
+                    email_from = msg['from']
+                    print 'From : ' + email_from + '\n'
+                    print 'Subject : ' + email_subject + '\n'
+                    print(msg.get_payload(decode=True))
+
+    except Exception, e:
+        print str(e)
+CheckNewEmails()
